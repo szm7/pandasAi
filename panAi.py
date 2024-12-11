00296d3ce1d02b1c4a9e26f0ceee98a1f3c0057c
szm7 <96779061+szm7@users.noreply.github.com>
2024-12-11 13:43:04 +0530

    fix

--- panAi.py
+++ panAi.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from pandasai import SmartDataframe
 import pandas as pd
 import os
 from langchain_groq import ChatGroq
@@ -21,26 +20,20 @@ df = pd.read_csv("Data.csv")
 st.set_page_config(page_title="Pandas AI with ChatGroq", layout="wide")
 
 st.title("Pandas AI with ChatGroq")
-
-# Preview of the Data
-df_preview = df.head()  # Display first few rows for preview
+df_preview = df.head() 
 st.write("### Preview of the Data")
 st.dataframe(df_preview)
 
-# Convert DataFrame to SmartDataframe
 #smart_df = SmartDataframe(df, config={"llm": llm,})
 agent = Agent(df,config={"llm": llm, "open_charts":False,"save_charts":False})
 agent.train(docs='if there is spelling mistake please find the most related values also dont generate any graphs')
 
-# Input Box for Querying the Data
 st.write("### Ask questions about your data")
 user_query = st.text_area("Enter your query:", "")
 
 if st.button("Run Query"):
     with st.spinner("Processing your query..."):
         try:
-            # Use SmartDataframe to process the query
-            
             rephrased_query = agent.rephrase_query(user_query)
             print("The rephrased query is", rephrased_query)
             result = agent.chat(rephrased_query)
